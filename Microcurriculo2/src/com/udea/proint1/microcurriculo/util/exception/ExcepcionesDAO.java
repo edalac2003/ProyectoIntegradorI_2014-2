accb0db87a9b9b7473c8e81c8e97a7464b583ac9
eacosta <eacosta@1TXTKW1>
2014-10-12 01:29:28 -0500

    Excepciones

--- Microcurriculo2/src/com/udea/proint1/microcurriculo/util/exception/ExcepcionesDAO.java
+++ Microcurriculo2/src/com/udea/proint1/microcurriculo/util/exception/ExcepcionesDAO.java
@@ -1,27 +1,27 @@
 package com.udea.proint1.microcurriculo.util.exception;
 
-public class Excepciones extends Exception {
+public class ExcepcionesDAO extends Exception {
 
-	public Excepciones() {
+	public ExcepcionesDAO() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Excepciones(String arg0) {
+	public ExcepcionesDAO(String arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Excepciones(Throwable arg0) {
+	public ExcepcionesDAO(Throwable arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Excepciones(String arg0, Throwable arg1) {
+	public ExcepcionesDAO(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 		// TODO Auto-generated constructor stub
 	}
 
-	public Excepciones(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+	public ExcepcionesDAO(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
 		// TODO Auto-generated constructor stub
 	}
---
+++ Microcurriculo2/src/com/udea/proint1/microcurriculo/util/exception/ExcepcionesLogica.java
@@ -0,0 +1,30 @@
+package com.udea.proint1.microcurriculo.util.exception;
+
+public class ExcepcionesLogica extends Exception {
+
+	public ExcepcionesLogica() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExcepcionesLogica(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExcepcionesLogica(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExcepcionesLogica(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExcepcionesLogica(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
