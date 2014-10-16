3dd53c9d436f069122a6b068f24b10e9e80af1e5
eacosta <eacosta@1TXTKW1>
2014-10-16 12:53:48 -0500

    Actualizacion Librerias

--- Microcurriculo2/src/com/udea/proint1/microcurriculo/dao/MicrocurriculosDAO.java
+++ Microcurriculo2/src/com/udea/proint1/microcurriculo/dao/MicrocurriculosDAO.java
@@ -5,15 +5,31 @@ import java.util.List;
 import com.udea.proint1.microcurriculo.dto.Microcurriculos;
 import com.udea.proint1.microcurriculo.util.exception.ExcepcionesDAO;
 
+/*
+ * Interfaz MicrocurriculosDAO
+ * 
+ *  Esta Interfaz define los metodos asociados a los Microcurriculos
+ *  
+ *  @Author.  Grupo Proyecto Integrador I. 2014-2
+ */
+
 public interface MicrocurriculosDAO {
 
 	public void guardarMicrocurriculo(Microcurriculos microcurriculo) throws ExcepcionesDAO;
 	
-	public Microcurriculos obtenerMicrocurriculo(String IdMicrocurriculo) throws ExcepcionesDAO;
+	public Microcurriculos obtenerMicrocurriculo(String idMicrocurriculo) throws ExcepcionesDAO;
+	
+	public void modificarMicrocurriculo (Microcurriculos microcurriculo) throws ExcepcionesDAO;
 	
 	public List<Microcurriculos> listarMicrocurriculos() throws ExcepcionesDAO;
 	
-	public void modificarMicrocurriculo (String idMicrocurriculo) throws ExcepcionesDAO;
+	public List<Microcurriculos> listarMicrocurriculosPorSemestre(String idSemestre) throws ExcepcionesDAO;
+	
+	public List<Microcurriculos> listarMicrocurriculosPorNucleo(String idNucleo) throws ExcepcionesDAO;
+	
+	public List<Microcurriculos> listarMicrocurriculosPorMateria(String idMateria) throws ExcepcionesDAO;
+	
+	public List<Microcurriculos> listarMicrocurriculosPorResponsable(String idResponsable) throws ExcepcionesDAO;
 	
 	
 }
--- Microcurriculo2/src/com/udea/proint1/microcurriculo/dao/hibernate/MicrocurriculosDAOHibernate.java
+++ Microcurriculo2/src/com/udea/proint1/microcurriculo/dao/hibernate/MicrocurriculosDAOHibernate.java
@@ -3,18 +3,28 @@ package com.udea.proint1.microcurriculo.dao.hibernate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.udea.proint1.microcurriculo.dao.MicrocurriculosDAO;
 import com.udea.proint1.microcurriculo.dto.Microcurriculos;
 import com.udea.proint1.microcurriculo.util.exception.ExcepcionesDAO;
 
-public class MicrocurriculosDAOHibernate extends HibernateDaoSupport implements MicrocurriculosDAO {
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(locations = "classpath:configuracion-spring.xml")
 
-	MicrocurriculosDAO microcurriculosDAO;
+public class MicrocurriculosDAOHibernate extends HibernateDaoSupport implements MicrocurriculosDAO {
 	
 	public MicrocurriculosDAOHibernate() {
 		// TODO Auto-generated constructor stub
@@ -28,20 +38,36 @@ public class MicrocurriculosDAOHibernate extends HibernateDaoSupport implements 
 	}
 
 	@Override
-	public Microcurriculos obtenerMicrocurriculo(String idMicrocurriculo)
+	public Microcurriculos obtenerMicrocurriculo(String IdMicrocurriculo)
 			throws ExcepcionesDAO {
-		
 		Session session = null;
 		Microcurriculos microcurriculo = null;
 		
 		try{
 			session = getSession();
-			microcurriculo = (Microcurriculos)session.get(Microcurriculos.class, idMicrocurriculo);
+			microcurriculo = (Microcurriculos)session.get(Microcurriculos.class, IdMicrocurriculo);
 			
 		}catch(HibernateException e){
 			throw new ExcepcionesDAO();
 		}
 		return microcurriculo;
+	}
+
+	@Override
+	public void modificarMicrocurriculo(Microcurriculos microcurriculo)
+			throws ExcepcionesDAO {
+		Session session = null;
+		Transaction tx = null;
+		
+		try{
+			session = getSession();
+			tx = session.beginTransaction();
+			session.update(microcurriculo);
+			tx.commit();
+			
+		}catch(HibernateException e){
+			throw new ExcepcionesDAO();
+		}
 	}
 
 	@Override
@@ -51,9 +77,11 @@ public class MicrocurriculosDAOHibernate extends HibernateDaoSupport implements 
 		
 		try{
 			session = getSession();
+			
 			Criteria criteria = session.createCriteria(Microcurriculos.class);
 			
 			microcurriculos = criteria.list();
+			
 		}catch(HibernateException e){
 			throw new ExcepcionesDAO();
 		}
@@ -62,10 +90,46 @@ public class MicrocurriculosDAOHibernate extends HibernateDaoSupport implements 
 	}
 
 	@Override
-	public void modificarMicrocurriculo(String idMicrocurriculo)
+	public List<Microcurriculos> listarMicrocurriculosPorSemestre(String idSemestre)
+			throws ExcepcionesDAO {
+		Session session = null;
+		List<Microcurriculos> microcurriculos = new ArrayList<Microcurriculos>();
+		
+		try{
+			session = getSession();
+			
+			Query query = session.createQuery("from Microcurriculos where idSemestre = :semestre");
+			
+			query.setEntity("Semestre", idSemestre);
+			
+			microcurriculos = query.list();
+			
+		}catch(HibernateException e){
+			throw new ExcepcionesDAO();
+		}
+		
+		return microcurriculos;
+	}
+
+	@Override
+	public List<Microcurriculos> listarMicrocurriculosPorNucleo(String idNucleo)
 			throws ExcepcionesDAO {
 		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public List<Microcurriculos> listarMicrocurriculosPorMateria(String idMateria)
+			throws ExcepcionesDAO {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Microcurriculos> listarMicrocurriculosPorResponsable(String idResponsable)
+			throws ExcepcionesDAO {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
