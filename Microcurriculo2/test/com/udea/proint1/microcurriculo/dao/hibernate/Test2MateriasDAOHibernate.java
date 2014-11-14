db9dfcde1ae3a57d64303136e9ce7de901e798b1
Elmer <Elmer@Elmer>
2014-11-13 23:35:57 -0500

    Ajustes en vista consultar microcurriculo

---
+++ Microcurriculo2/test/com/udea/proint1/microcurriculo/dao/hibernate/Test2MateriasDAOHibernate.java
@@ -0,0 +1,35 @@
+package com.udea.proint1.microcurriculo.dao.hibernate;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.udea.proint1.microcurriculo.dao.MateriasDAO;
+import com.udea.proint1.microcurriculo.dto.TbAdmMaterias;
+import com.udea.proint1.microcurriculo.util.exception.ExcepcionesDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(locations = "classpath:configuracion-spring.xml")
+public class Test2MateriasDAOHibernate {
+
+	@Autowired
+	MateriasDAO materiasDAO;
+	
+	@Test
+	public void testBuscarMaterias() {
+		try {
+			for(TbAdmMaterias materia: materiasDAO.buscarMaterias("201001%")){
+				System.out.println("materia: "+ materia.getVrNombre());
+			}
+		} catch (ExcepcionesDAO e) {
+			System.out.println(e);
+		}
+	}
+
+}
